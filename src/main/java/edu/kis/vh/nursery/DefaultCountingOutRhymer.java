f1c8f83c53d62ec5344d78d6682c0d96f228d7e5
Sadlence <weronika8754@gmail.com>
2025-03-27 18:06:10 +0100

    6. Powiązanie ze sobą stałej MAX_SIZE i total. Dodanie stałej RETURN dla domyślnie zwracanej wartości. 7. Ustawienie modyfikatora final w plikach Node.java i FIFORhymer.java

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,6 +4,8 @@ public class DefaultCountingOutRhymer {
 
     public static final int MAX_SIZE = 12;
     public static final int INIT_VAL = -1;
+    public static final int RETURN = -1;
+
     private final int[] numbers = new int[MAX_SIZE];
 
     public int total = INIT_VAL;
@@ -18,18 +20,18 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_SIZE - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return RETURN;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return RETURN;
         return numbers[total--];
     }
 
